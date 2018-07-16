e7bd6a70062575fc3c4502f66224c55d50519d6a
llqhz <llqhz@qq.com>
2018-07-16 15:06:21 +0800

    sss

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'llqhz\\' => array($baseDir . '/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitbe6a3835326e8328ad4ee91e9a538714
 {
+    public static $prefixLengthsPsr4 = array (
+        'l' => 
+        array (
+            'llqhz\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'llqhz\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInitbe6a3835326e8328ad4ee91e9a538714::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitbe6a3835326e8328ad4ee91e9a538714::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
