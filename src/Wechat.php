e267819f387062ff3533af03720226a0c5628c0d
llqhz <llqhz@qq.com>
2018-07-16 15:03:00 +0800

    first commit

---
+++ src/Wechat.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @Author: name
+ * @Date:   2018-07-16 14:57:10
+ * @Last Modified by:   name
+ * @Last Modified time: 2018-07-16 15:01:16
+ */
+namespace llqhz;
+
+
+/**
+* A
+*/
+class WeChat
+{
+    public function info()
+    {
+        return 'llqhz@qq.com';
+    }
+
+}
