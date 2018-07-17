ae522ef120b674220f22661647aca3f7c296eb67
llqhz <llqhz@qq.com>
2018-07-17 10:24:15 +0800

    测试 webhooks

--- src/WeChat2.php
+++ src/WeChat2.php
@@ -3,7 +3,7 @@
  * @Author: name
  * @Date:   2018-07-16 14:57:10
  * @Last Modified by:   name
- * @Last Modified time: 2018-07-16 15:20:24
+ * @Last Modified time: 2018-07-17 10:23:35
  */
 namespace llqhz;
 
